0293117f43662c742afa2da851443bd34cc326c8
ptan01 <ptanjim01@gmail.com>
2023-05-02 11:16:56 +0600

    data created

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express() ;
+const port = process.env.PORT || 5000 ;
+
+const chefs = require('./data/chefinfo.json');
+
+app.get('/', (req, res)=>{
+    res.send('the Kitchen server')
+})
+
+app.get('/chefs',(req, res)=>{
+    res.send(chefs)
+})
+
+app.get('/chefs/:id', (req , res)=>{
+    const id = req.params.id ;
+    const singleChef = chefs.find(s => s.id === id);
+    res.send(singleChef)
+})
+
+app.listen(port , ()=>{
+    console.log('the server is running port', port)
+})
