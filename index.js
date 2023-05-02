0eba7128491e73f311f627548ec16703508a0daf
ptan01 <ptanjim01@gmail.com>
2023-05-02 11:43:30 +0600

    install cors and app use cors

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express() ;
+const cors = require('cors')
 const port = process.env.PORT || 5000 ;
 
 const chefs = require('./data/chefinfo.json');
-
+app.use(cors())
 app.get('/', (req, res)=>{
     res.send('the Kitchen server')
 })
